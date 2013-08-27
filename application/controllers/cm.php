a2583b6bf5b11863808803dcab1257fb2e11be15
Bukky <miaravindh@gmail.com>
2013-08-27 18:12:42 +0530

    star

--- application/controllers/cm.php
+++ application/controllers/cm.php
@@ -50,7 +50,7 @@ class Cm extends CI_Controller {
 		'dd_dropdowns' => array('fellowship_id', 'sub_fellowship_id'),
 		//SETUP URL POST FOR EACH CHILD
 		//List in order as per above
-		'dd_url' => array('', site_url() . '/cm/sub_asso/'),
+		'dd_url' => array('', site_url() . 'cm/sub_asso/'),
 		//LOADER THAT GETS DISPLAYED NEXT TO THE PARENT DROPDOWN WHILE THE CHILD LOADS
 		'dd_ajax_loader' => base_url('assets/admin/img') . '/' . 'ajax-loader.gif');
 		$output -> dropdown_setup = $dd_data;
@@ -80,7 +80,7 @@ class Cm extends CI_Controller {
 			$sub_fellowship_ID = $row -> sub_fellowship_id;
 
 			//GET THE STATES PER COUNTRY ID
-			$this -> db -> select('*') -> from('sub_fellowships') -> where('sub_fellowship_id', $sub_fellowship_ID);
+			$this -> db -> select('*') -> from('sub_fellowships') -> where('fellowship_id', $sub_fellowship_ID);
 			$db = $this -> db -> get();
 			/*
 			 if($db->result() > 0){
@@ -153,6 +153,7 @@ class Cm extends CI_Controller {
 		$crud -> set_relation('participant_district', 'districts', 'district_name') -> set_relation('participant_association', 'fellowships', 'fellowship_name');
 		$crud -> required_fields('participant_name', 'participant_chess', 'participant_church', 'participant_association', 'participant_district');
 		$output = $crud -> render();
+		$output -> menu = 'Participants';
 		$this -> _layout_output($output);
 	}
 
@@ -165,14 +166,135 @@ class Cm extends CI_Controller {
 		-> display_as('district_id', 'District') 
 		-> display_as('participant_id', 'Name & Chess') 
 		-> display_as('fellowship_id', 'Association')
-		-> display_as('fellowship_id', 'Association') 
+		-> display_as('sub_fellowship_id', 'Sub Association') 
 		-> display_as('item_id', 'Item Name');
-		$crud -> fields('participant_id','district_id', 'fellowship_id', 'sub_fellowship_id', 'item_id');
+		$crud -> fields('district_id', 'participant_id', 'fellowship_id', 'sub_fellowship_id', 'item_id');
 		$crud -> required_fields('district_name');
-		$crud -> set_relation('participant_id', 'participants', '{participant_chess} - {participant_name}');
+		$crud -> set_relation('participant_id', 'participants', '{participant_chess} - {participant_name}')
+		-> set_relation('district_id', 'districts', 'district_name')
+		-> set_relation('fellowship_id', 'fellowships', 'fellowship_name')
+		-> set_relation('item_id', 'items', 'item_name')
+		-> set_relation('sub_fellowship_id', 'sub_fellowships', 'sub_fellowship_name');
+		///$output = $crud -> render();
+		//$this -> _layout_output($output);
+		//$crud -> callback_add_field('participant_id', array($this, 'empty_subpar_dropdown_select'));
+		//$crud -> callback_edit_field('participant_id', array($this, 'empty_subpar_dropdown_select'));
+		
+		$crud -> callback_add_field('sub_fellowship_id', array($this, 'empty_subaso_dropdown_select'));
+		$crud -> callback_edit_field('sub_fellowship_id', array($this, 'empty_subaso_dropdown_select'));
+        
+        $crud -> callback_add_field('item_id', array($this, 'empty_item_dropdown_select'));
+		$crud -> callback_edit_field('item_id', array($this, 'empty_item_dropdown_select'));
+		
 		$output = $crud -> render();
-		$output -> menu = 'Districts';
+
+		$dd_data = array(
+		//GET THE STATE OF THE CURRENT PAGE - E.G LIST | ADD
+		'dd_state' => $crud -> getState(),
+		//SETUP YOUR DROPDOWNS
+		//Parent field item always listed first in array, in this case countryID
+		//Child field items need to follow in order, e.g stateID then cityID
+		'dd_dropdowns' => array('district_id','participant_id','fellowship_id', 'sub_fellowship_id' ,'item_id'),
+		//SETUP URL POST FOR EACH CHILD
+		//List in order as per above
+		'dd_url' => array('',site_url() . 'cm/sub_part/',site_url() . 'cm/all_asso/', site_url() . 'cm/sub_asso/',site_url().'cm/sub_items/'),
+		//LOADER THAT GETS DISPLAYED NEXT TO THE PARENT DROPDOWN WHILE THE CHILD LOADS
+		'dd_ajax_loader' => base_url('assets/admin/img') . '/' . 'ajax-loader.gif');
+		$output -> dropdown_setup = $dd_data;
+		$output -> menu = 'Participants';
 		$this -> _layout_output($output);
+	}
+
+	function empty_item_dropdown_select() {
+		//CREATE THE EMPTY SELECT STRING
+		$empty_select = '<select name="item_id" class="chosen-select" data-placeholder="Competition Item" style="width: 300px; display: none;">';
+		$empty_select_closed = '</select>';
+		//GET THE ID OF THE LISTING USING URI
+		$listingID = $this -> uri -> segment(4);
+
+		//LOAD GCRUD AND GET THE STATE
+		$crud = new grocery_CRUD();
+		$state = $crud -> getState();
+
+		//CHECK FOR A URI VALUE AND MAKE SURE ITS ON THE EDIT STATE
+		if (isset($listingID) && $state == "edit") {
+			//GET THE STORED STATE ID
+			$this -> db -> select('sub_fellowship_id,item_id') -> from('participants_items') -> where('participants_items_id', $listingID);
+			$db = $this -> db -> get();
+			$row = $db -> row(0);
+			//var_dump($row);
+			//$item_ID = $row -> item_id;
+			$sub_fellowship_ID = $row -> sub_fellowship_id;
+
+			//GET THE STATES PER COUNTRY ID
+			$this -> db -> select('*') -> from('items') -> where('sub_fellowship_id', $sub_fellowship_ID);
+			$db = $this -> db -> get();
+			/*
+			 if($db->result() > 0){
+
+			 $crud -> required_fields('sub_fellowship_id');
+			 }
+			 */
+			//APPEND THE OPTION FIELDS WITH VALUES FROM THE STATES PER THE COUNTRY ID
+			foreach ($db->result() as $row) :
+				if ($row -> sub_fellowship_id == $sub_fellowship_ID) {
+					$empty_select .= '<option value="' . $row -> item_id . '" selected="selected">' . $row -> item_name . '</option>';
+				} else {
+					$empty_select .= '<option value="' . $row -> item_id . '">' . $row -> item_name . '</option>';
+				}
+			endforeach;
+
+			//RETURN SELECTION COMBO
+			return $empty_select . $empty_select_closed;
+		} else {
+			//RETURN SELECTION COMBO
+			return $empty_select . $empty_select_closed;
+		}
+	}
+function empty_subpar_dropdown_select() {
+		//CREATE THE EMPTY SELECT STRING
+		$empty_select = '<select name="participant_id" class="chosen-select" data-placeholder="Paticipants" style="width: 300px; display: none;">';
+		$empty_select_closed = '</select>';
+		//GET THE ID OF THE LISTING USING URI
+		$listingID = $this -> uri -> segment(4);
+
+		//LOAD GCRUD AND GET THE STATE
+		$crud = new grocery_CRUD();
+		$state = $crud -> getState();
+
+		//CHECK FOR A URI VALUE AND MAKE SURE ITS ON THE EDIT STATE
+		if (isset($listingID) && $state == "edit") {
+			//GET THE STORED STATE ID
+			$this -> db -> select('district_id') -> from('participants_items') -> where('participant_id', $listingID);
+			$db = $this -> db -> get();
+			$row = $db -> row(0);
+			//var_dump($row);
+			//$item_ID = $row -> item_id;
+			$district_ID = $row -> district_id;
+			//GET THE STATES PER COUNTRY ID
+			$this -> db -> select('participant_id,participant_name,participant_chess') -> from('participants') -> where('participant_district', $district_ID);
+			$db = $this -> db -> get();
+			/*
+			 if($db->result() > 0){
+
+			 $crud -> required_fields('sub_fellowship_id');
+			 }
+			 */
+			//APPEND THE OPTION FIELDS WITH VALUES FROM THE STATES PER THE COUNTRY ID
+			foreach ($db->result() as $row) :
+				if ($row -> district_id == $district_ID) {
+					$empty_select .= '<option value="' . $row -> participant_id . '" selected="selected">' .$row -> participant_chess.'-'. $row -> participant_name . '</option>';
+				} else {
+					$empty_select .= '<option value="' . $row -> participant_id . '">' .$row -> participant_chess.'-'. $row -> participant_name . '</option>';
+				}
+			endforeach;
+
+			//RETURN SELECTION COMBO
+			return $empty_select . $empty_select_closed;
+		} else {
+			//RETURN SELECTION COMBO
+			return $empty_select . $empty_select_closed;
+		}
 	}
 
 	//GET JSON OF STATES
@@ -181,12 +303,74 @@ class Cm extends CI_Controller {
 
 		$this -> db -> select("*") -> from('sub_fellowships') -> where('fellowship_id', $fellowsip_id);
 		$db = $this -> db -> get();
-
-		$array = array();
+		
+        $array = array();
+        if(count($db->result())>0){
 		foreach ($db->result() as $row) :
 			$array[] = array("value" => $row -> sub_fellowship_id, "property" => $row -> sub_fellowship_name);
 		endforeach;
+		}else{
+			
+			$array[] = array("value" => "no".$fellowsip_id, "property" => "No record");
+		}
+		echo json_encode($array);
+		exit ;
+	}
+	
+	function sub_part() {
+		$participant_id = $this -> uri -> segment(3);
 
+		$this -> db -> select("*") -> from('participants') -> where('participant_district', $participant_id);
+		$db = $this -> db -> get();
+		
+        $array = array();
+		foreach ($db->result() as $row) :
+			$array[] = array("value" => $row -> participant_id, "property" => $row -> participant_name);
+		endforeach;
+		echo json_encode($array);
+		exit ;
+	}
+	function all_asso() {
+		$participant_id = $this -> uri -> segment(3);
+
+		$this -> db -> select("*") -> from('fellowships');
+		$db = $this -> db -> get();
+		
+        $array = array();
+		foreach ($db->result() as $row) :
+			$array[] = array("value" => $row -> fellowship_id, "property" => $row -> fellowship_name);
+		endforeach;
+		echo json_encode($array);
+		exit ;
+	}
+	
+	function sub_items() {
+		$item_id = $this -> uri -> segment(3);
+		
+		
+        $val_check= substr($item_id, 0,2);
+		if($val_check != "no"){
+			$this -> db -> select("*") -> from('items') -> where('sub_fellowship_id', $item_id);
+			$db = $this -> db -> get();
+        
+			$array = array();
+			foreach ($db->result() as $row) :
+				$array[] = array("value" => $row -> item_id, "property" => $row -> item_name);
+			endforeach;
+			
+		}else{
+			
+			$val= substr($item_id, 2);
+			
+			$this -> db -> select("*") -> from('items') -> where('fellowship_id', $val);
+			$db = $this -> db -> get();
+        
+			$array = array();
+			foreach ($db->result() as $row) :
+				$array[] = array("value" => $row -> item_id, "property" => $row -> item_name);
+			endforeach;
+			
+		}
 		echo json_encode($array);
 		exit ;
 	}
